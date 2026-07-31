150a3d459ce56d540e0821831e40ad8e18d6b1c6
Ayeshasaif17 <aishacheema2004@gmail.com>
2026-07-31 19:42:16 +0200

    task13

---
+++ task13.py
@@ -0,0 +1,10 @@
+marks = {}
+x = int(input("Enter the marks of phy:"))
+marks.update({"phy": x})
+
+x=int(input("Enter the marks of chem:"))
+marks.update({"chem": x})   
+
+x=int(input("Enter the marks of math:"))
+marks.update({"math": x})   
+print(marks)
