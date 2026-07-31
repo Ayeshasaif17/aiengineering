c1d07290e040ec464abaea2e7e48499e6682dd00
Ayeshasaif17 <aishacheema2004@gmail.com>
2026-07-31 11:59:27 +0200

    task7

---
+++ task7.py
@@ -0,0 +1,12 @@
+#name of three favoruite movies and store in list
+movies = []
+
+movie1 = input("Enter the name of your favorite movie: ")
+movie2 = input("Enter the name of your second favorite movie: ")
+movie3 = input("Enter the name of your third favorite movie: ")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+    
+print(movies)
