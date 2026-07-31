a33fb95b1e24f97197ea60b3caa520de54d5cf02
Ayeshasaif17 <aishacheema2004@gmail.com>
2026-07-31 09:43:05 +0200

    updated

--- firstprogram.py
+++ firstprogram.py
@@ -1,3 +1,3 @@
-side=float (input("Enter the side length of the square: "))
+side=float (input("Enter the side length of the square: "))#Area of a Square
 area=side*side
 print("The area of the square is:", area)
